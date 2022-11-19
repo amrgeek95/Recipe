240bc51f4527fa824a3204199fadd84138196ee7
amrgeek95 <amr.geek.95@gmail.com>
2022-11-19 16:28:50 +0200

    change app key because it finished .

--- Reciepe/Constants/Constant.swift
+++ Reciepe/Constants/Constant.swift
@@ -9,6 +9,6 @@ import Foundation
 import UIKit
 
  let mainStoryBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-let app_id = "b08f19a2"
-let app_key = "46e21ed917d1c46243aa767cde8bbd96"
+let app_id = "298292b6"
+let app_key = "8d3d6ea1d584c76716e7ef894d62f181"
 let sourcesURL = "https://api.edamam.com/api/recipes/v2?type=public&app_id=\(app_id)&app_key=\(app_key)&field=uri&field=label&field=image&field=url&field=ingredientLines"
